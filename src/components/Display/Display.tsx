372e0536a2d801918435bec522adf61d1133c054
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-02 23:49:03 +0100

    Add functionality to Display component

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,7 +1,10 @@
+import { useAppSelector } from "../../store/hooks";
 import styles from "./Display.module.scss";
 
 const Display = (): JSX.Element => {
-  return <span className={styles.display}></span>;
+  const displayValue = useAppSelector((state) => state.calculator.display);
+
+  return <span className={styles.display}>{displayValue}</span>;
 };
 
 export default Display;
