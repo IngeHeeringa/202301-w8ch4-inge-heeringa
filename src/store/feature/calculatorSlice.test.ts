7d5381706b9b5b613b08e6f1fb5e10c2f59414a7
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-03 00:24:45 +0100

    Test calculatorReducer - sumNumbers

--- src/store/feature/calculatorSlice.test.ts
+++ src/store/feature/calculatorSlice.test.ts
@@ -2,6 +2,7 @@ import { CalculatorState } from "../../types";
 import {
   appendNumberActionCreator,
   calculatorReducer,
+  sumNumbersActionCreator,
 } from "./calculatorSlice";
 
 describe("Given a calculatorReducer function", () => {
@@ -9,21 +10,46 @@ describe("Given a calculatorReducer function", () => {
     test("Then it should return a new state with display 538 and currentOperand 538", () => {
       const currentState: CalculatorState = {
         display: "53",
-        currentOperand: "53",
-        previousOperand: "",
+        currentOperand: 53,
+        previousOperand: 0,
         operator: null,
       };
       const payload = "8";
       const expectedState: CalculatorState = {
         display: "538",
-        currentOperand: "538",
-        previousOperand: "",
+        currentOperand: 538,
+        previousOperand: 0,
         operator: null,
       };
 
       const newState = calculatorReducer(
         currentState,
         appendNumberActionCreator(payload)
+      );
+
+      expect(newState).toStrictEqual(expectedState);
+    });
+  });
+
+  describe("When it receives the number 6 and a sumNumbers action with payload 4", () => {
+    test("Then it should return a new state with display 10, currentOperand 10 and previousOperand 6", () => {
+      const currentState: CalculatorState = {
+        display: "6",
+        currentOperand: 6,
+        previousOperand: 0,
+        operator: null,
+      };
+      const payload = "4";
+      const expectedState: CalculatorState = {
+        display: "10",
+        currentOperand: 10,
+        previousOperand: 6,
+        operator: null,
+      };
+
+      const newState = calculatorReducer(
+        currentState,
+        sumNumbersActionCreator(payload)
       );
 
       expect(newState).toStrictEqual(expectedState);
